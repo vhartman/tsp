503c1dd0d171f2303272d2ba48c948fd3f5a9baf
Valentin Hartmann <vhartman@student.ethz.ch>
2019-11-30 01:42:58 +0100

    Add testing, fix nn

--- tsp.py
+++ tsp.py
@@ -1,16 +1,18 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 import random
 
 import kdtree
 
 def nn(dots):
-    ordered_dots = []
     not_visited = dots.tolist()
 
     current = not_visited[0]
     del not_visited[0]
 
-    tree = kdtree.create(not_visited[1:])
+    ordered_dots = [current]
+    tree = kdtree.create(not_visited)
 
     while tree.data is not None:
         nearest_info = tree.search_nn(current)
@@ -20,14 +22,14 @@ def nn(dots):
         current = nearest
 
         tree = tree.remove(current)
-        
+
     return np.vstack(ordered_dots)
 
 def simulated_annealing(dots):
     def path_length(path):
         c = 0
-        for i in range(len(path) - 1):
-            c += np.linalg.norm(np.array(path[i]) - np.array(path[i+1]))
+        for i in range(len(path)):
+            c += np.linalg.norm(np.array(path[i-1]) - np.array(path[i]))
 
         return c
 
@@ -47,60 +49,36 @@ def simulated_annealing(dots):
     random.shuffle(path)
     
     T = 1000
+    alpha = 0.2
     while True:
-        change = False
-        for _ in range(len(path)):
+        for _ in range(len(path)*10):
             c = path_length(path) 
-            print(c)
 
             start, end = get_rand_ind(len(path))
-            r = np.random.rand()
-            if r > 0.5:
-                # transport
-                path_cp = path[:start]
-                path_cp.extend(path[end:])
-
-                #print('transporting')
-                #print(len(path))
-                #print(len(path_cp))
-
-                splice_pos = np.random.randint(0, len(path_cp))
-                seg = path[start:end]
-                #print(len(seg))
-                #print(len(seg) + len(path_cp))
-                #print()
-
-                for i in range(len(seg)):
-                    path_cp.insert(splice_pos + i, seg[i])
-                
-            else:
-                #reverse
+            
+            if True:
                 path_cp = path[:]
-                path_cp[start:end] = path_cp[end:start:-1]
-
-                #print('reverse')
-                #print(len(path_cp))
-                #print(len(path))
-
-            #print('lens')
-            #print(len(path))
-            #print(len(path_cp))
-            #print()
+                seg = path[start:end]
+                seg = seg[::-1]
+                path_cp[start:end] = seg
+            else:
+                path_cp = path[:]
+                path_cp[start] = path[end]
+                path_cp[end] = path[start]
 
             nc = path_length(path_cp)
             if nc < c:
                 path = path_cp
-                change = True
             else:
                 diff = nc - c
 
-                if np.exp(diff/T) > np.random.rand():
-                    path = path_cp
-                    change = True
+                #print(T,np.exp(-diff/T), start, end)
 
-        T = T-1
-        print(T)
-        if not change:
+                if np.exp(-diff/T) > np.random.rand():
+                    path = path_cp
+
+        T = T * (1 - alpha)
+        if T <= 2.5:
             break
 
     return np.vstack(path)
@@ -116,3 +94,32 @@ def tsp(dots, style='nn'):
         return simulated_annealing(dots)
 
     return None
+
+def show_tsp(dots, ax=None):
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+
+    dots = np.vstack([dots, dots[0, :]])
+    ax.plot(dots[:, 1], -dots[:, 0], linewidth=1)
+
+if __name__ == "__main__":
+    np.random.seed(10)
+
+    n = 100
+    pts = np.random.rand(n, 2) * 1000
+
+    ordered_pts_nn = tsp(pts, style='nn')
+    ordered_pts_sa = tsp(pts, style='sa')
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(1,2,1)
+    ax.scatter(pts[:, 1], -pts[:, 0])
+    show_tsp(ordered_pts_nn, ax)
+
+    ax = fig.add_subplot(1,2,2)
+    ax.scatter(pts[:, 1], -pts[:, 0])
+    show_tsp(ordered_pts_sa, ax)
+
+    plt.show()
